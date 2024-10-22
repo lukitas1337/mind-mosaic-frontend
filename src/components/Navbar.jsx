295a260de7531726a6d009137c5ac4290243ef89
sogol mofrad <sogolmofrad@gmail.com>
2024-10-22 12:58:46 +0200

    import fonts in the css file

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,22 +1,33 @@
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
     <nav className="navBar flex justify-between items-center px-10 py-6 border-[#1e1e1e] border-b-[1px]">
       <div>
         <p>
-        <Link to="/" className="text-2xl font-normal font-italiana border-[#1e1e1e]">mind Mosaic</Link>
+          <Link
+            to="/"
+            className="text-2xl font-normal font-italiana border-[#1e1e1e]"
+          >
+            mind Mosaic
+          </Link>
         </p>
       </div>
       <ul className="navLinks text-base flex gap-5 font-monda border-[#1e1e1e]">
         <li>
-          <Link to="/" className="hover:text-gray-600">HOME</Link>
+          <Link to="/" className="hover:text-gray-600">
+            HOME
+          </Link>
         </li>
         <li>
-          <Link to="/blog" className="hover:text-gray-600">BLOG</Link>
+          <Link to="/blog" className="hover:text-gray-600">
+            BLOG
+          </Link>
         </li>
         <li>
-          <Link to="/contact" className="hover:text-gray-600">CONTACT</Link>
+          <Link to="/contact" className="hover:text-gray-600">
+            CONTACT
+          </Link>
         </li>
       </ul>
     </nav>
