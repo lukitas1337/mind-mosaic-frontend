72d940fc6c75b0ed64b121f11d8c783dc925896e
Lukitas <lvd.follert@gmail.com>
2024-10-22 12:26:23 +0200

    home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,11 +1,21 @@
 const Home = () => {
-  return (
-    <>
-    <h1>MY BLOG</h1>
-    <p>A journey through thoughts, stories, and the art of living authentically.</p>
-    <img src="mind-mosaic-frontend/public/home-image.jpeg" alt="handwritten notebook on a table" />
-    </>
-  )
-}
-
-export default Home
+    return (
+      <>
+        <section className="homeContainer flex flex-col justify-center items-center text-center">
+            <div className="mt-24 mb-14">
+                <h1 className="text-4xl font-italiana">MY BLOG</h1>
+                <p className="text-md font-italiana">
+                    A journey through thoughts, stories, and the art of living authentically.
+                </p>
+          </div>
+          <img
+            className="max-w-[832px] max-h-[546px] object-cover"
+            src="./public/home-image.jpeg"
+            alt="handwritten notebook on a table"
+          />
+        </section>
+      </>
+    );
+  }
+  
+  export default Home;
