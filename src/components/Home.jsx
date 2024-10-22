57c65506ade8f26fc7e5a7a5c4b965ff0f02a5a4
Lukitas <lvd.follert@gmail.com>
2024-10-22 12:05:43 +0200

    basic setup

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,10 @@
 const Home = () => {
   return (
-    <div>Home</div>
+    <>
+    <h1>MY BLOG</h1>
+    <p>A journey through thoughts, stories, and the art of living authentically.</p>
+    <img src="mind-mosaic-frontend/public/home-image.jpeg" alt="handwritten notebook on a table" />
+    </>
   )
 }
 
