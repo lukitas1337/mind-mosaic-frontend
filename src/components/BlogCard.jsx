be8dfef2127906fb5984462a5dbe7a9daf671722
Sofia Ex <dev.exarchou@outlook.com>
2024-10-22 14:08:55 +0200

    create grid add basic styles

--- src/components/BlogCard.jsx
+++ src/components/BlogCard.jsx
@@ -1,8 +1,10 @@
 export const BlogCard = ({ title, content, date, readMoreButton }) => (
-    <div>
-        <h1>{title}</h1>
-        <p>{content}</p>
-        <p>{new Date(date).toLocaleDateString()}</p>
-        <button>{readMoreButton}</button>
+    <div className="bg-white rounded-lg shadow-lg p-6 m-4 max-w-xs">
+        <h1 className="text-xl font-bold mt-4">
+            {title}
+        </h1>
+        <p className="mt-4">{content}</p>
+        <p className="text-sm text-gray-600">{new Date(date).toLocaleDateString()}</p>
+        <button className="text-white bg-black">{readMoreButton}</button>
     </div>
 );
--- src/components/BlogCardsView.jsx
+++ src/components/BlogCardsView.jsx
@@ -1,7 +1,7 @@
 import { BlogCard } from "./BlogCard";
 
 export const BlogCardsView = ({ posts }) => (
-    <div>
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {posts.map(post => (
             <BlogCard
                 key={post.id}
