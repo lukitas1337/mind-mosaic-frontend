d8c8917e3e05b90bfec21de964f2ee642397c373
Sylwia Kruk <sylviaamj@gmail.com>
2024-10-23 21:36:20 +0200

    added edit post

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from "./components/Navbar";
 import Home from "./components/Home";
 import Blog from "./components/Blog";
 import Contact from "./components/Contact";
+import EditBlogForm from "./components/EditBlogForm";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/update/:id" element={<EditBlogForm />} />
       </Routes>
     </Router>
   );
